dbb9b7b8140ba8b9a0c7ccc2a2d16463dbb5a3e9
Matheus Alende <165348908+Mathyess@users.noreply.github.com>
2025-08-26 21:11:01 -0300

    Update rabbit population simulation logic
    Refactored the main function to separately track young and adult rabbits, simulate monthly population growth, and added a predation phase that reduces the population by 25% each month until extinction.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,26 @@
 void main() {
-  int coelhos = 2;
+  int jovens = 1;
+  int adultos = 1;
   int meses = 12;
-  int coelhosjovens= 2;
-  for (int i = 1; i <= meses; i++) {
-    coelhos *= 2;
-    print('Mês $i: $coelhos coelhos');
+
+  for (int mes = 1; mes <= meses; mes++) {
+    int novosJovens = adultos * 2;
+    adultos += jovens;
+    jovens = novosJovens;
+    int total = jovens + adultos;
+    print(
+      'Fim do Mês $mes: População de coelhos: $total, Jovens $jovens, Adultos $adultos',
+    );
+  }
+
+  int totalCoelhos = jovens + adultos;
+  int mesPredacao = 1;
+  while (totalCoelhos > 0) {
+    totalCoelhos = (totalCoelhos * 0.75).floor();
+    print(
+      'Após predação - Mês $mesPredacao: População de coelhos: $totalCoelhos',
+    );
+    mesPredacao++;
+    if (totalCoelhos == 0) break;
   }
 }
